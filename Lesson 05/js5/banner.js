71ec2f25dcd3ff50f68957ec2f74ba7ff8271d1e
goodday1983 <tip19007@byui.edu>
2021-05-22 15:44:31 -0600

    updated

--- Lesson 05/js5/banner.js
+++ Lesson 05/js5/banner.js
@@ -1,4 +1,3 @@
 const daynames = new Date();
-if(daynames.getDay() == 6){
-    document.querySelector('#friday').style.display = 'block';
-} else{document.querySelector('#friday').style.display ="none"}
+if(daynames.getDay() == 5){
+    document.querySelector('#friday').style.display = 'block'; } 
