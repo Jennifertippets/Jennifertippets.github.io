7a029d1a6337953a6c44c7ddd2c1978d780a2cc9
goodday1983 <tip19007@byui.edu>
2021-05-22 23:28:33 -0600

    updated home

--- Lesson 04/js4/app4.js
+++ Lesson 04/js4/app4.js
@@ -4,3 +4,8 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 	now
 );
 datefield.innerHTML = `<em>${fulldate}</em>`;
+
+function toggleMenu() {
+	console.log(document.getElementById("menu").classList);
+	document.getElementById("menu").classList.toggle("hide");
+}
