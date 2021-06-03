af0cd875de01727af5ab206c8423ff43aecdfba9
goodday1983 <tip19007@byui.edu>
2021-06-02 22:29:25 -0600

    updated

--- Lesson 07/js7/lazyload.js
+++ Lesson 07/js7/lazyload.js
@@ -9,7 +9,7 @@ function preloadImage(img) {
 }
 
 const imgOptions = {
-    threshold: 0,
+    threshold: 1,
     rootMargin: "0px 0px 50px 0px"
 };
 const imgObserver = new IntersectionObserver((entries,imgObserver) => {entries.forEach(entry => {
