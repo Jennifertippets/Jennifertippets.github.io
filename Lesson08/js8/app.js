c83bf52f11a5fe089ea0d1c0888830cfa557f910
goodday1983 <tip19007@byui.edu>
2021-06-09 01:09:50 -0600

    updated

---
+++ Lesson08/js8/app.js
@@ -0,0 +1,15 @@
+const datefield = document.querySelector("#date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+function toggleMenu() {
+	console.log(document.getElementById("menu").classList);
+	document.getElementById("menu").classList.toggle("hide");
+}
+
+WebFont.load({google: {families: ["Fira Sans", "Padauk"]
+    }});
