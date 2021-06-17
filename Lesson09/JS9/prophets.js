edbad732f9ce40455f843171a28e077b6e8d14f6
goodday1983 <tip19007@byui.edu>
2021-06-16 21:00:21 -0600

    updated

---
+++ Lesson09/JS9/prophets.js
@@ -0,0 +1,29 @@
+const requestURL ='https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {  
+    /*console.table(jsonObject);*/
+    const prophets = jsonObject['prophets'];
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let birthday = document.createElement('p');
+        let birthplace = document.createElement('p');
+
+
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+birthday.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+birthplace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+
+
+card.appendChild(h2);
+card.appendChild(birthday);
+card.appendChild(bithplace);
+
+
+document.querySelector('div.cards').appendChild(card);
+    }
+
+  });
