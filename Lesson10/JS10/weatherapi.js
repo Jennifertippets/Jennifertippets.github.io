2b275b1dbdc28d10b481247aa07db2aa75a9ac7e
goodday1983 <tip19007@byui.edu>
2021-06-25 00:56:22 -0600

    updated

--- Lesson10/JS10/weatherapi.js
+++ Lesson10/JS10/weatherapi.js
@@ -3,10 +3,10 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp;
-    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-const desc = jsObject.weather[0].description;  // note how we reference the weather array
-document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
-document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
-document.getElementById('icon').setAttribute('alt', desc);
+    document.getElementById('currently').textContent = jsObject.weather[0].description.toUpperCase();
+    document.getElementById('temp').textContent = jsObject.main.temp.toFixed(0);
+    document.getElementById('humidity').textContent = jsObject.main.humidity;
+    document.getElementById('speed').textContent = jsObject.wind.speed;
   });
+
+  
---
+++ Lesson10/JS10/weatherapi0.js
@@ -0,0 +1,12 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=92d23e29b4ff25e703f6841c5f01cbaa&units=imperial";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0);
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+const desc = jsObject.weather[0].description;  // note how we reference the weather array
+document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+document.getElementById('icon').setAttribute('alt', desc);
+  });
