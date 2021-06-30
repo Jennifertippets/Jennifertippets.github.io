c92642c8538473fae65c0b6b36f43728c7d0013b
goodday1983 <tip19007@byui.edu>
2021-06-30 01:15:08 -0600

    updated

--- Lesson10/JS10/app.js
+++ Lesson10/JS10/app.js
@@ -5,7 +5,15 @@ const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
 );
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
+/**************bar menu************/
 
+/*const menubutton = document.querySelector('.menu');
+const principalnav = document.querySelector('.hide')
+
+menubutton.addEventListener('click', () => {principalnav.classList.toggle('responsive')}, false);
+
+
+/*window.onresize = () => {if (window.innerWidth > 760) principalnav.classList.remove('responsive')};*/
 function toggleMenu() {
 	console.log(document.getElementById("menu").classList);
 	document.getElementById("menu").classList.toggle("hide");
