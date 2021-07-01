e7ba2d88c4348a39037b7c9d239f8fbc18c2ee35
goodday1983 <tip19007@byui.edu>
2021-07-01 01:38:51 -0600

    updated

--- Lesson11/JS/sodaspringsapi.js
+++ Lesson11/JS/sodaspringsapi.js
@@ -21,4 +21,36 @@ console.log(speedW);
    else{
       document.getElementById('wind').textContent = "N/A";
    }
-  }); 
+  });
+  
+  
+  let cityID = 5607916;
+  let appid = '92d23e29b4ff25e703f6841c5f01cbaa';
+  const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&APPID=${appid}&units=imperial`;
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let day = 0;  
+    const dayofweek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const fiveDayForecast = jsObject.list.filter(forecast => forecast.dt_txt.includes('18:00:00'));
+    console.log(fiveDayForecast);
+
+    fiveDayForecast.forEach(x => {
+      let d = new Date(x.dt_txt);
+
+      document.getElementById(`dayofweek${day+1}`).textContent = dayofweek[d.getDay()];
+      document.getElementById(`forecast${day+1}`).textContent = x.main.temp.toFixed(0);
+     
+      let days = x.weather[0].icon;
+
+      const imagesrc = 'https://openweathermap.org/img/wn/' + days + '@2x.png'; 
+      const desc = x.weather[0].description;
+    
+    
+      document.getElementById(`image${day + 1}`).setAttribute('src', imagesrc); 
+     document.getElementById(`image${day + 1}`).setAttribute('alt', desc);
+    day++;
+  });
+ });
+
