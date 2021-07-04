9998b04a944b58c41c9e476ee8182e4c5c4556e4
goodday1983 <tip19007@byui.edu>
2021-07-03 22:36:03 -0600

    updated

--- Lesson11/JS/app.js
+++ Lesson11/JS/app.js
@@ -59,16 +59,7 @@ if ('IntersectionObserver' in window) {
   });
 }
 
-if(typeof Storage!=="undefined"){
-    if(localStorage.visitcount) {
-        document.getElementById("visit").innerHTML = "Welcome back, you've been here " + localStorage.visitcount  +  " times before.";
-        localStorage.visitcount = Number(localStorage.visitcount) + 1;
-    } else {
-        localStorage.visitcount = 1;
-        document.getElementById("visit").innerHTML = "Welcome. This is your first time here!";
 
-    }
-    }
 console.log("localstorage visit count now:" + localStorage.visitcount);
 
   /************************storm center************************* */  
