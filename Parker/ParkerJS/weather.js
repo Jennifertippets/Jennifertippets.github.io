77839894dbed136919c3d4f56942805b77fbcc62
goodday1983 <tip19007@byui.edu>
2021-07-17 03:36:01 -0600

    updated

--- Parker/ParkerJS/weather.js
+++ Parker/ParkerJS/weather.js
@@ -1,5 +1,10 @@
-(function($) {
-	$('.weather .close ').click(function() {
-	  $(this).parent().fadeOut();
-	});
-  })(jQuery);
+const oneapiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=39.519&lon=-104.761&exclude=minutely,hourly&appid=92d23e29b4ff25e703f6841c5f01cbaa&units=imperial";
+fetch(oneapiURL)
+.then((response) => response.json())
+.then((data) => {
+	console.log(data);
+	document.getElementById('currently').textContent = data.current.weather[0].description.toUpperCase();
+    document.getElementById('temp').textContent = data.current.temp.toFixed(0);
+    document.getElementById('humidity').textContent = data.current.humidity;
+	document.getElementById('alert').textContent = data.alerts[0].event;
+});
