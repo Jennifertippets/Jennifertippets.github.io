e8e3fb4692ed0a0d7a3c32f852358a546f3a5296
goodday1983 <tip19007@byui.edu>
2021-07-21 02:17:12 -0600

    updated

--- Parker/ParkerJS/grid-list.js
+++ Parker/ParkerJS/grid-list.js
@@ -1,25 +1,19 @@
-var company=document.getElementsByClassName("section");
+var element = document.getElementsByClassName("company");
+
+// Declare a loop variable
 var i;
 
-function listView(){
-     for(i=0;i<company.length;i++){
-         company[i].style.width="100%";
-     }
+// List View
+function listView() {
+  for (i = 0; i < element.length; i++) {
+    element[i].style.width = "100%";
+  }
 }
 
-function gridView(){
-     for(i=0;i<company.length;i++){
-         company[i].style.width="50%";
-     }
+// Grid View
+function gridView() {
+  for (i = 0; i < element.length; i++) {
+    element[i].style.width = "50%";
+  }
 }
 
-var container=document.getElementById("buttons-one");
-var btns=container.getElementsByClassName("buttons-two");
-
-for( var i=0; i<btns.length; i++){
-    btns[i].addEventListener("click", function(){
-        var current=document.getElementsByClassName("active");
-        current[0].className=current[0].className.replace("active","");
-        this.className+=" active";
-    })
-}
