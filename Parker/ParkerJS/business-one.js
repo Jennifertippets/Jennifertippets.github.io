96e902e1e4b964f36242ff4279636cbb35699851
goodday1983 <tip19007@byui.edu>
2021-07-21 14:32:17 -0600

    updated

--- Parker/ParkerJS/business-one.js
+++ Parker/ParkerJS/business-one.js
@@ -6,7 +6,7 @@ fetch('./JSON/Parker.json')
 	.then(function (Object) {
 		/*console.log(Object);*/
 		const Businesses = Object['Businesses'];
-        const business = Businesses.filter(x => x.name == 'Havana Bakery and Cafe' || x.name == 'Cinderella Ranch Boutique' || x.name == 'Kilwins Chocolate and Ice Cream') 
+        const business = Businesses.filter(x => x.name == 'Havana Bakery and Cafe' || x.name == 'Cinderella Ranch Boutique' || x.name == 'Parker Garage') 
 		for (let i = 0; i < business.length; i++){
 			let companies = document.createElement('section');
 			let h2 = document.createElement('h2');
